9cc1515a3848c7885b3035281c6cb90216708bb5
ducnguyen <ducnn.230288@gmail.com>
2024-03-07 09:37:17 +0700

    - Update image elements to use lazy loading and add script for handling collapsible elements with animation. - Add event listeners for different message types and implement dynamic content rendering using Handlebars in the template.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
 import './less.less'
 
-// vanilla-lazyload 17
-new LazyLoad({callback_error: (el: any) => (el.src = 'https://via.placeholder.com/1x1/?text=') });
-
-// glightbox 3
-GLightbox({});
-
 // sweetalert2 11
 import { Message } from './message.ts'
 import { API } from './api.ts'
+(<any>window).API = API;
+(<any>window).Message = Message;
 
 import { setupFormValid } from './validation.ts'
 setupFormValid();
-
-(<any>window).API = API;
-(<any>window).Message = Message;
 (<any>window).SetupFormValid = setupFormValid;
 (<any>window)._MESSAGE_ = {
   required: 'Xin vui lòng nhập nội dung',
@@ -25,50 +18,15 @@ setupFormValid();
   compare: 'Xin vui lòng nhập không quá ',
 };
 
-
-
-
-
 import { setupMenuMobile } from './menu-mobile.ts';
 setupMenuMobile(document.getElementsByClassName('handle-menu'));
 // swiper 10
 import { setupSwiper } from './swiper.ts';
 setupSwiper();
 
-
-
-
-const temp = document.getElementById("template-1");
-if (temp) {
-  const template = Handlebars.compile(temp.innerHTML);
-  const data = {"datass": JSON.stringify({ "name": "Alan", "hometown": "Somewhere, TX",
-      "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]}), "name": "Alan", "hometown": "Somewhere, TX",
-    "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}] };
-  document.getElementById('text')!.innerHTML = template(data);
-}
-
-Array.from(document.getElementsByClassName('handle-dialog')).forEach((el) => el.addEventListener('click', () =>  {
-  const temp = document.getElementById("template-1");
-  if (temp) {
-    const template = Handlebars.compile(temp.innerHTML);
-    const data = { "name": "Alan", "hometown": "Somewhere, TX",
-      "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
-    Message.html(template(data));
-  }
-
-}))
-
 // echarts 5
 import { setupEchart } from './echart.ts'
 setupEchart();
-
-// inputmask 5
-Inputmask().mask(document.querySelectorAll("input"));
-
-document.getElementById('success')!.addEventListener('click', () => Message.success({text: 'Success'}))
-document.getElementById('warning')!.addEventListener('click', () => Message.warning({text: 'Warning'}))
-document.getElementById('confirm')!.addEventListener('click', () => Message.confirm({text: 'Confirm'}))
-document.getElementById('error')!.addEventListener('click', () => Message.error({text: 'Error'}))
 
 window.onload = async () => {
   const test = await API.post('/authentication/jwt/login', {
@@ -82,18 +40,3 @@ window.onload = async () => {
   });
   console.log(test);
 };
-
-const time: any = {};
-document.querySelectorAll('.handle-collapse').forEach((e: any) => e.addEventListener('click', () => {
-  if (!time[e.id]) time[e.id] = gsap.timeline({defaults: {duration: 0.25, ease: 'power1.inOut'}});
-  else {
-    time[e.id][time[e.id].reversed() ? 'play' : 'reverse']();
-    return
-  }
-
-  time[e.id]
-    .to(e.parentElement.querySelector('.handle-content'), {height: 'auto'}, '0')
-  time[e.id]
-    .to(e.querySelector('.la-angle-down'), { transform: 'rotate(0deg)'}, '0');
-
-}));
