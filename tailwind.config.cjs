f7b661a971a6c4fe7fd0745e547ea05d9c180c08
ducnn230288 <ducnn230288@gmail.com>
2024-03-12 04:58:21 +0700

    Update heading styles and blockquote design.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./*.html'],
   safelist: ['!border', '!border-solid', '!border-teal-900', '!bg-white', '!text-teal-900', 'hover:!bg-none', 'right-0', 'opacity-50'],
   theme: {
+    extend: {
+      fontSize: {
+        xs: ['.6875rem', '.9375rem'],
+        sm: ['.8125rem', '1.1875rem'],
+      },
+    },
     screens: {
       sm: '640px',
       md: '768px',
