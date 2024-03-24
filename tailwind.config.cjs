0a61665b43e693018beb638271d8cf5890bbf0d6
ducnn230288 <ducnn230288@gmail.com>
2024-03-24 09:32:06 +0700

    Add preconnect links for Google Fonts and update font families in the CSS file.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
   safelist: ['!border', '!border-solid', '!border-teal-900', '!bg-white', '!text-teal-900', 'hover:!bg-none', 'right-0', 'opacity-50'],
   theme: {
     extend: {
+
       fontSize: {
         xs: ['.6875rem', '.9375rem'],
         sm: ['.8125rem', '1.1875rem'],
       },
+    },
+    fontFamily: {
+      'sans': ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+      'serif': ['Playfair Display', ...defaultTheme.fontFamily.serif],
     },
     screens: {
       sm: '640px',
